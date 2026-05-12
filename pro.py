9a3a1b5cb9c7f04c8533cdf6043fb813b77a6cdb
anjaligozra4-lang <anjaligozra4@gmail.com>
2026-05-12 10:40:06 +0530

    today is my 6th lecture

---
+++ pro.py
@@ -0,0 +1,173 @@
+# print("anjali")
+
+# # anjali = input("enter the number")
+# # print("your number is-",anjali)
+
+
+# # variable =23 
+# # print(variable)
+
+# # variable = "ha"
+# # print("variable")
+# ab = "school" 
+# print(ab[0:4])
+
+# ab = "school"
+# for l in ab:
+#     print(l)
+
+# # list
+
+# ab =["school","uni","hostel"]
+
+# student = ["anjali","mannat","anamjot","satnam","navjot"]
+# print(student)
+
+# print(type (student))
+
+# student = ["anjali","mannat","navjot","navjot"]
+# print(student)
+
+# student = ["anjali","mannat","navjot","navjot"]
+# student[3]="satnam"
+# print(student)
+
+# student = ["anjali","mannat","navjot","navjot"]
+# print(student[3])
+
+
+# student = ["anjali","mannat","navjot","satnam"]
+# student.insert(1,23)
+# print(student)
+
+# student = ["anjali","mannat","navjot","satnam"]
+# student.remove("anjali")
+# print(student)
+
+# student = ["anjali","mannat","navjot","satnam"]
+# student.pop(1)
+# print(len(student))
+
+
+# # tuple
+
+# ab = ("table","chair",2,3,4)
+# print(ab)
+# print(type(ab))
+# print(len(ab))
+
+# # set
+# ab = {"table","chair",2,3,4,"table"}
+# print(ab)
+# print(type(ab))
+# print(len(ab))
+
+# ab = {"table","chair",2,3,4,"table"}
+# ab.add(6)
+# print(ab)
+# print(type(ab))
+# print(len(ab))
+
+# ab = {"table","chair",2,3,4,"table"}
+# ab.remove("table")
+# print(ab)
+# print(type(ab))
+# print(len(ab))
+
+# email=["anjali@gmail.com","vanshika@gmail.com","mannat@gmail.com","anjali@gmail.com","navjot@gmail.com"]
+# print(email)
+# print(set(email))
+
+# # dictionary 
+# student={"id":"45",
+#          "name":"anjali",
+#          "class":"b.tech",
+#          "roll":"23"}
+# # print(student)
+# # print(type(student))
+# # print((student["roll"]))
+# # student["id"]=56
+# # print(student)
+
+# del student["class"]
+# print(student)
+
+# # typecasting
+# a="10"
+# b="12"
+# c=int(b)
+# print(type(a))
+# print(float(b))
+# print(type(c))
+
+# function
+# def std_name():
+#     print("anjali")
+# std_name() 
+
+# function with parameter
+# def std_name(a,b):
+#     c=a+b
+#     print(c)
+# std_name(5,6)
+
+# def std_name(name):
+#     print(name)
+# std_name("anjali") 
+
+# def std_name(valu):
+#     print(valu)
+
+# std_name("10") 
+# std_name("17") 
+# std_name("14") 
+# std_name("13") 
+
+
+# def std_name(q,l):
+#     area = q * l
+#     return area
+    
+# school=std_name(9,6)
+# print(school)
+...
+
+# a=int(input("enter first number"))
+# b=int(input("enter second number"))
+# def std_name(a,b):
+#     area=a*b
+#     return area
+# school=std_name(a,b)
+# print(school)
+
+# lambda function
+# ab=lambda x:x*x
+# print(ab(6)) 
+...
+# file handling
+
+# file = open("data.txt","r")
+# school=file.read()
+# print(school)
+# file.close()
+
+file = open("data.txt","w")
+school=file.write("b.tech cse")
+print(school)
+file.close()
+
+# if elif else
+marks = 85
+if marks>=90:
+    print("grade A")
+elif marks>=50:
+      print("grade B")
+elif marks>=50:
+     print("grade C")
+else:
+     print("Fail")
+                   
+
+
+
+
